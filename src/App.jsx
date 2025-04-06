3eb25e71c2e8b3bc4d8e4592a0c6473dde92a8bd
mikeadex <joko.adeleye@gmail.com>
2025-04-06 17:37:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { HelmetProvider } from 'react-helmet-async';
+import AppNavigation from './navigation/app-navigation';
+
+function App() {
+
+  return (
+    <HelmetProvider>
+      <AppNavigation/>
+    </HelmetProvider>
+  )
+}
+
+export default App;
